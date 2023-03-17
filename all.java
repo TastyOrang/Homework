8df61b943eb9604168aeb649eac5317bcb7da4df
TastyOrang <128151803+TastyOrang@users.noreply.github.com>
2023-03-18 02:51:54 +0300

    Add files via upload

---
+++ all.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.util.Scanner;
+
+public class all {
+
+	public static void main(String[] args) {
+		
+		Scanner a = new Scanner(System.in);
+		
+		System.out.println("Enter a five-digit number");
+		int number = a.nextInt();
+		int number1 = number/10000;
+		int number2 = number%10000/1000;
+		int number3 = number%10000%1000/100;
+		int number4 = number%10000%1000%100/10;
+		int number5 = number%10000%1000%100%10/1;
+		
+		System.out.println(number1);
+		System.out.println(number2);
+		System.out.println(number3);
+		System.out.println(number4);
+		System.out.println(number5);
+		
+
+	}
+
+}
